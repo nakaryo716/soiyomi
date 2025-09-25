e8a6ebf544fa63e1488ae602ee31e4eb307caf79
Ryo Nakaya <128471892+nakaryo716@users.noreply.github.com>
2025-09-25 19:54:59 +0900

    feat: 音声再生機能を追加 (#1)

--- src/audio_player.rs
+++ src/audio_player.rs
@@ -1,1 +1,25 @@
+use std::{fs::File, io::BufReader, path::PathBuf, sync::mpsc::Receiver};
 
+use rodio::mixer::Mixer;
+
+pub struct AudioPlayer<'a> {
+    path_receiver: Receiver<PathBuf>,
+    mixer: &'a Mixer,
+}
+
+impl<'a> AudioPlayer<'a> {
+    pub fn new(path_receiver: Receiver<PathBuf>, mixer: &'a Mixer) -> Self {
+        AudioPlayer {
+            path_receiver,
+            mixer,
+        }
+    }
+
+    pub fn run(&mut self) {
+        while let Ok(path) = self.path_receiver.recv() {
+            let input = BufReader::new(File::open(path).unwrap());
+            let sink = rodio::play(self.mixer, input).unwrap();
+            sink.sleep_until_end();
+        }
+    }
+}
